b122cddd777a03add3760bad98fbcf9a699aa676
endrol <luodamingsisu@gmail.com>
2022-03-03 23:22:24 +0900

    update

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -14,8 +14,6 @@ class UNet(nn.Module):
         self.down3 = Down(256, 512)
         factor = 2 if bilinear else 1
         self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
         self.up3 = Up(256, 128 // factor, bilinear)
         self.up4 = Up(128, 64, bilinear)
         self.outc = OutConv(64, n_classes)
